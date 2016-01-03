241682814115af64b33e4bbd75c40cd53df11844
Yacine Mekesser <ymekesser@hotmail.com>
2016-01-03 23:38:20 +0100

    Reformat Code and added a more interesting demo animation (expanding sphere)

--- first_test.py
+++ first_test.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from visual import *
+import math
 
 # Parameters
 gap = 12
@@ -15,23 +16,38 @@ wireMaterial = materials.plastic
 
 # Setup
 leds = []
-offset = vector(xCount, yCount, zCount) / -2 # Offset to center cube
+offset = vector(xCount, yCount, zCount) / -2  # Offset to center cube
 for xPos in range(xCount):
     for yPos in range(yCount):
         for zPos in range(zCount):
             pos = (vector(xPos, yPos, zPos) + offset) * gap
             color = (xPos / xCount, yPos / yCount, zPos / zCount)
-            newLed = sphere(pos=pos, radius=radius, color=color, opacity=opacity, material = material)
+            newLed = sphere(pos=pos, radius=radius, color=color, opacity=opacity, material=material)
             leds.append(newLed)
 
 if wires:
     for xPos in range(xCount):
         for zPos in range(zCount):
             pos = (vector(xPos, 0, zPos) + offset) * gap
-            cylinder(pos=pos, axis=(0, (yCount - 1) * gap, 0), radius=wireRadius, color=wireColor, material = wireMaterial)
+            cylinder(pos=pos, axis=(0, (yCount - 1) * gap, 0), radius=wireRadius, color=wireColor,
+                     material=wireMaterial)
 
 # Algorithms
-while(True):
+r = 0
+d = 15
+s = 0.01
+while (True):
     for led in leds:
-        led.color = (1, 0, 0)
-        sleep(0.05)
+        assert (isinstance(led, sphere))
+        distance = math.sqrt(reduce(lambda x, y: x + y, map(lambda x: math.pow(x - offset[0], 2), led.pos)))
+        print str(distance) + " " + str(r) + " " + str(d)
+        if distance < r and distance > (r - d):
+            led.color = (1, 0, 0)
+        else:
+            led.color = (0.1, 0.1, 0.1)
+    if r < xCount * gap:
+        r = r + 5
+    else:
+        r = 0
+
+    sleep(s)
