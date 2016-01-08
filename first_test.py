acdfa36b1e80a08093161456afac516f7ef66926
Joris Morger <ymekesser@hotmail.com>
2016-01-08 18:48:09 +0100

    abstracted a bit

--- first_test.py
+++ first_test.py
@@ -2,6 +2,7 @@ from __future__ import division
 from visual import *
 import math
 import time
+from random import randint
 
 # Parameters
 gap = 12
@@ -14,6 +15,12 @@ wires = True
 wireRadius = 0.1
 wireColor = color.gray(0.1)
 wireMaterial = materials.plastic
+
+WIDTH = 8
+HEIGHT = 8
+DEPTH = 8
+red = (1,0,0)
+s = 0.03
 
 # Setup
 leds = []
@@ -34,22 +41,53 @@ if wires:
                      material=wireMaterial)
 
 # Algorithms
-r = 0
-d = 15
-s = 0.01
+
+# def updateNeopixelStrip(strip, leds):
+#     strip = neopixelstrip
+#     for idx, led in leds:
+#         color = Color(led.color.x * 255.0, led.color.y * 255.0, led.color.z * 255.0)
+#         strip.setPixelColor(idx, color)
+
+def setLedPixelColor(led, color, brightness):
+    led.color = color
+    led.opacity = brightness
+
+def getCoords(i):
+    (restW, width) = divmod(i, WIDTH)
+    (restH, height) = divmod(restW, HEIGHT)
+    (blub, depth) = divmod(restH, DEPTH)
+    return (width, height, depth)
+
+def randomAlgo(i, r, d):
+    (x, y, z) = getCoords(i)
+    distance = randint(0,255)
+    if distance < r and distance > (r - d):
+        return (i, (x, y, z), red, 1.0)
+    else:
+        return (i, (x, y, z), (0.1,0.1,0.1), 1.0)
+
+def runAlgo(frameCounter):
+    r = 0
+    d = 15
+    animation = []
+    for i in range(frameCounter):
+        animation.append([randomAlgo(i, r, d) for i in range(WIDTH * HEIGHT * DEPTH)])
+        if r < xCount * gap:
+            r = r + 5
+        else:
+            r = 0
+    return animation
+
+def animateFrame(leds, frame):
+    for l, (i,pos,c,_) in zip(leds, frame):
+        setLedPixelColor(l, c, 1.0)
+    time.sleep(s)
+
+
+def applyAlgo(leds, animation):
+    map(lambda frame: animateFrame(leds, frame), animation)
 
 while (True):
-    for led in leds:
-        assert (isinstance(led, sphere))
-        distance = math.sqrt(reduce(lambda x, y: x + y, map(lambda x: math.pow(x - offset[0], 2), led.pos)))
-        print str(distance) + " " + str(r) + " " + str(d)
-        if distance < r and distance > (r - d):
-            led.color = (1, 0, 0)
-        else:
-            led.color = (0.1, 0.1, 0.1)
-    if r < xCount * gap:
-        r = r + 5
-    else:
-        r = 0
-
-    time.sleep(s)
+    animation = runAlgo(100)
+    print 'animation done.'
+    applyAlgo(leds, animation)
