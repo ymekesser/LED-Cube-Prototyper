ee38b67eac59555de8388d3bccf0de5a8b3dd92b
Joris Morger <ymekesser@hotmail.com>
2016-01-12 19:51:06 +0100

    improved readability

--- first_test.py
+++ first_test.py
@@ -20,28 +20,51 @@ WIDTH = 8
 HEIGHT = 8
 DEPTH = 8
 red = (1,0,0)
+green = (0,1,0)
+blue = (0,0,1)
 s = 0.03
-ITERATIONS = 100 #how many times do we iterate the algo?
+ITERATIONS = 50 #how many times do we iterate the algo?
+leds = []
 
 # Setup
-leds = []
-offset = vector(xCount, yCount, zCount) / -2  # Offset to center cube
-for xPos in range(xCount):
-    for yPos in range(yCount):
-        for zPos in range(zCount):
-            pos = (vector(xPos, yPos, zPos) + offset) * gap
-            color = (xPos / xCount, yPos / yCount, zPos / zCount)
-            newLed = sphere(pos=pos, radius=radius, color=color, opacity=opacity, material=material)
-            leds.append(newLed)
-
-if wires:
+def setup():
+    offset = vector(xCount, yCount, zCount) / -2  # Offset to center cube
     for xPos in range(xCount):
-        for zPos in range(zCount):
-            pos = (vector(xPos, 0, zPos) + offset) * gap
-            cylinder(pos=pos, axis=(0, (yCount - 1) * gap, 0), radius=wireRadius, color=wireColor,
-                     material=wireMaterial)
+        for yPos in range(yCount):
+            for zPos in range(zCount):
+                pos = (vector(xPos, yPos, zPos) + offset) * gap
+                color = (xPos / xCount, yPos / yCount, zPos / zCount)
+                newLed = sphere(pos=pos, radius=radius, color=color, opacity=opacity, material=material)
+                leds.append(newLed)
+
+    if wires:
+        for xPos in range(xCount):
+            for zPos in range(zCount):
+                pos = (vector(xPos, 0, zPos) + offset) * gap
+                cylinder(pos=pos, axis=(0, (yCount - 1) * gap, 0), radius=wireRadius, color=wireColor,
+                         material=wireMaterial)
 
 # Algorithms
+# i is the the position of the led (0-512)
+def insideOut(i):
+    (x, y, z) = getCoords(i)
+    distance = randint(0,255)
+    if x < distance % 9:
+        return (i, (x, y, z), red, 1.0)
+    elif y < distance % 9:
+        return (i, (x, y, z), blue, 1.0)
+    elif z < distance % 9:
+        return (i, (x, y, z), green, 1.0)
+    else:
+        return (i, (x, y, z), (0.1,0.1,0.1), 1.0)
+
+def randomAlgo(i):
+    (x, y, z) = getCoords(i)
+    distance = randint(0,255)
+    if distance < 20:
+        return (i, (x, y, z), red, 1.0)
+    else:
+        return (i, (x, y, z), (0.1,0.1,0.1), 1.0)
 
 # def updateNeopixelStrip(strip, leds):
 #     strip = neopixelstrip
@@ -60,29 +83,20 @@ def getCoords(i):
     (_, depth) = divmod(restH, DEPTH)
     return (width, height, depth)
 
-# i is the the position of the led (0-512) r and d are legacy. todo: think about
-# what makes sense here
-def randomAlgo(i, r, d):
-    (x, y, z) = getCoords(i)
-    distance = randint(0,255)
-    if distance < r and distance > (r - d):
-        return (i, (x, y, z), red, 1.0)
-    else:
-        return (i, (x, y, z), (0.1,0.1,0.1), 1.0)
 
-def perform( fun, *arg ):
-    return fun( *arg )
+# apply
+def apply(fun, *arg):
+    return fun(*arg)
 
+# pass an algorithm (Int -> Frame) and an Integer how many frames there are.
+# type Color = (Double, Double, Double)
+# type Position = (Int, Int, Int)
+# type LEDIndex = Brightness = Int
+# type Frame = (LEDIndex, Position, Color, Brightness)
 def runAlgo(algo, frameCounter):
-    r = 0
-    d = 15
     animation = []
     for i in range(frameCounter):
-        animation.append([perform(algo, i, r, d) for i in range(WIDTH * HEIGHT * DEPTH)])
-        if r < xCount * gap:
-            r = r + 5
-        else:
-            r = 0
+        animation.append([apply(algo, i) for i in range(WIDTH * HEIGHT * DEPTH)])
     return animation
 
 def animateFrame(leds, frame):
@@ -90,10 +104,13 @@ def animateFrame(leds, frame):
         setLedPixelColor(l, c, 1.0)
     time.sleep(s)
 
-def applyAnimation(leds, animation):
+def runAnimation(leds, animation):
     map(lambda frame: animateFrame(leds, frame), animation)
 
+setup()
 while (True):
+    animation = runAlgo(insideOut, ITERATIONS)
+    runAnimation(leds, animation)
     animation = runAlgo(randomAlgo, ITERATIONS)
+    runAnimation(leds, animation)
     print 'animation done.'
-    applyAnimation(leds, animation)
